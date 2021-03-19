30ce7f06365df809a4624998bf11b4b94c9bcbe8
Edward Cheadle <edward.cheadle8@gmail.com>
2021-03-19 15:15:03 -0600

    Move ship both directions

--- game_functions.py
+++ game_functions.py
@@ -11,12 +11,15 @@ def check_events(ship):
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 # Move ship to the right.
-                print(f'ship moving right {event.key}')
                 ship.moving_right = True
-            elif event.key == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = True
 
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
 
 def update_screen(ai_settings, screen, ship):
     """Update images on the screen and flip to new screen"""
