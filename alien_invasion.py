c822e3a7affdf9f71647a7c69a0cfc578fb2c870
Edward Cheadle <edward.cheadle8@gmail.com>
2021-03-19 10:48:25 -0600

    Move check event code to game_functions.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,7 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
-
+import game_functions as gf
 
 def run_game():
     # Initialize game and create a screen object.
@@ -20,11 +20,8 @@ def run_game():
     
     # Start the main loop for the game
     while True:
-
         # Watch for keyboard an mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events()
         # Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
         ship.blitme()
