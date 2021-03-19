ab3d7f5a6f373ef07e4211228b8ea77d48cf7933
Edward Cheadle <edward.cheadle8@gmail.com>
2021-03-19 16:36:23 -0600

    Add bullet shooting capacity

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -15,6 +16,8 @@ def run_game():
 
     # Make a Ship
     ship = Ship(ai_settings, screen)
+    # Make group to store bullets in
+    bullets = Group()
 
     # Set the background color
     # bg_color = (230, 230, 230)
@@ -22,9 +25,9 @@ def run_game():
     # Start the main loop for the game
     while True:
         # Watch for keyboard an mouse events.
-        gf.check_events(ship)
-
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
+        bullets_update()
         # print(f' x position: {ship.rect.centerx}')
         gf.update_screen(ai_settings, screen, ship)
 
